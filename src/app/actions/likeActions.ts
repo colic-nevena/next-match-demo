e52299b3c667dbed25dccc9548ba75bd95d793ae
colic-nevena <mrsnevenac@gmail.com>
2024-09-23 10:06:20 +0200

    Added: lists and tabs for likes

--- src/app/actions/likeActions.ts
+++ src/app/actions/likeActions.ts
@@ -48,3 +48,81 @@ export async function fetchCurrentUserLikeIds() {
         throw error;
     }
 }
+
+export async function getLikedMembers(type = 'source') {
+    try {
+        const userId = await getAuthUserId()
+
+        switch (type) {
+            case 'source':
+                return await getSourceLikes(userId)
+
+            case 'target':
+                return await getTargetLikes(userId)
+
+            case 'mutual':
+                return await getMutualLikes(userId)
+
+            default:
+                return []
+        }
+    } catch (error) {
+        console.log(error)
+        throw error;
+    }
+}
+
+async function getSourceLikes(userId: string) {
+    try {
+        const sourceLikes = await prisma.like.findMany({
+            where: { sourceUserId: userId },
+            select: { targetMember: true }
+        })
+
+        return sourceLikes.map(x => x.targetMember)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+async function getTargetLikes(userId: string) {
+    try {
+        const targetLikes = await prisma.like.findMany({
+            where: { targetUserId: userId },
+            select: { sourceMember: true }
+        })
+
+        return targetLikes.map(x => x.sourceMember)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+async function getMutualLikes(userId: string) {
+    try {
+        // those who I liked
+        const likedUsers = await prisma.like.findMany({
+            where: { sourceUserId: userId },
+            select: { targetUserId: true }
+        })
+
+        const likedIds = likedUsers.map(x => x.targetUserId)
+
+        // among those who I liked, find those who liked me back, where I am the target user
+        const mutualLikes = await prisma.like.findMany({
+            where: {
+                AND: [
+                    { targetUserId: userId },
+                    { sourceUserId: { in: likedIds } }
+                ]
+            },
+            select: { sourceMember: true }
+        })
+
+        return mutualLikes.map(x => x.sourceMember)
+    } catch (error) {
+        console.log(error)
+    }
+}
--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react'
+import ListsTab from './ListsTab'
+import { fetchCurrentUserLikeIds, getLikedMembers } from '../actions/likeActions'
 
-const ListsPage = () => {
-  return <div>ListsPage</div>;
-};
+export const dynamic = 'force-dynamic';
 
-export default ListsPage;
+export default async function ListsPage({ searchParams }: { searchParams: { type: string } }) {
+  const likeIds = await fetchCurrentUserLikeIds();
+  const members = await getLikedMembers(searchParams.type);
+
+  return (
+    <div>
+      <ListsTab members={members!} likeIds={likeIds} />
+    </div>
+  )
+}
